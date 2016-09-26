b30ab31055d87afa402331060ebd66af330359fb
RonyVidaur <rony.vidaur@gmail.com>
2016-09-26 10:08:20 -0600

    add and remove directories

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # learnyounode
-learnyounode exercises from nodeschool
+-learnyounode exercises from nodeschool
+-multiple simple exercises on NodeJS 
--- readWriteFiles.js
+++ readWriteFiles.js
@@ -1,10 +1,27 @@
 let fs = require('fs')
-
+/* this is for reading files
 let result = fs.readFileSync('test.txt','utf8')
 console.log(result)
+writing what we just read from the other file into another
 fs.writeFileSync('writeMe.txt', result)
 
-//ASYNC version
+//ASYNC version of the process (non-blocking)
 fs.readFile('test2.txt','utf8', (err, data) => {
   fs.writeFile('writeMe.txt',data)
+})
+--------------------------------------------
+delete a file
+fs.unlink('test.txt')
+*/
+
+//creating directories Sync version
+fs.mkdirSync('stuff')
+//deleting directories Sync version
+fs.rmdirSync('stuff')
+
+//ASYNC version and modifying files
+fs.mkdir('stuff',function(){
+  fs.readFile('test2.txt','utf8',function(err,data){
+    fs.writeFile('./stuff/test.txt')
+  })
 })
